da79d315da780c940bce4dd1e31cabb219849f35
Jacob Schroll <jacoblschroll@gmail.com>
2025-07-05 17:09:41 -0500

    Shift add implemented

--- src/project.v
+++ src/project.v
@@ -16,12 +16,28 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  reg [15:0] acc;
+  reg [3:0] count;
+
+  always @ (posedge clk)
+  begin
+    if (!rst_n) 
+    begin
+      acc <= 0;
+      count <= 0;
+    end
+    else 
+    begin
+      if (uio_in[count] == 1'b1 and count < 8) acc <= acc + (ui_in << count);
+      if (count < 8) count <= count + 1;
+    end
+  end
+
+  assign uo_out  = 0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
